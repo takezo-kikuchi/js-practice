2dd01943637a15bcafebd7fdb631c821a135dac5
takezo-kikuchi <tkiku1414@gmail.com>
2024-10-10 09:55:58 +0900

    first commit

---
+++ my_calculator/script.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const myParseFloat = (str) => {
+    let index = 0;
+    let sign = 1;
+    let num = 0;
+    let decimalPlace = 0;
+    let hasDecimal = false;
+
+    while (index < str.length && str[index] === " ") index++;
+
+    if (str[index] === "+") index++;
+    else if (str[index] === "-") {
+        sign = -1;
+        index++;
+    }   
+
+    while (index < str.length) {
+        const char = str[index];
+        if (char >= '0' && char <= '9') {
+          num = num * 10 + (char.charCodeAt(0) - '0'.charCodeAt(0));
+        } else if (char === '.' && !hasDecimal) {
+          hasDecimal = true;
+          decimalPlace = 1/10;
+        } else {
+          break; // Stop if an invalid character is found
+        }
+        if (hasDecimal) decimalPlace *= 10;
+        index++;
+      }
+    if (decimalPlace > 0) num = num / decimalPlace;
+    return sign * num;
+}
+
+console.log(myParseFloat("123.45")); // 123.45
+
+const myEval = (formula) => {
+    const operator = formula.match(/[^0-9.]/g);
+    let result;
+    // constしているのはa,bのみ　arrayではない
+    let [a,b] = formula.split(operator[0]);
+    a = myParseFloat(a)
+    b = myParseFloat(b)
+    if (operator == "+") {
+        result = a+b  }
+    else if (operator == "-") {
+        result = a-b }
+    else if (operator == "*") {
+        result = a*b }
+    else if (operator == "/"){
+        // 0で割るとエラーになるので例外処理
+        result = a/b
+    }
+    else throw "Error"
+    return result
+}
+document.getElementById("button2").addEventListener("click", function(event) {
+    event.preventDefault();  // Prevents form submission
+    let formula = document.getElementById("formula-input").value;
+    try {
+        let result = myEval(formula);
+        document.getElementById("result").textContent = result;
+    } catch (e) {
+        console.log(e);
+        document.getElementById("result").textContent = "Error in formula!";
+    }
+});
